2d5aa4dc6f33d60c78c4d4c6939289f8d9f9028f
Jackson Hardaker <jacksonhardaker@lyft.com>
2022-08-02 10:57:13 -0400

    sort paths before concat

--- main.rs
+++ main.rs
@@ -3,15 +3,19 @@ use std::fs::File;
 use std::io::Write;
 
 fn main() {
-    let paths = fs::read_dir("./log").unwrap();
+    let mut paths: Vec<_> = fs::read_dir("./log")
+                                .unwrap()
+                                .map(|r| r.unwrap())
+                                .collect();
+    paths.sort_by_key(|dir| dir.path());
     let mut contents = String::new();
     contents.push_str("# Renovation 2022\n\n");
 
     for path in paths {
-        let file_content = fs::read_to_string(&path.unwrap().path())
+        let file_content = fs::read_to_string(&path.path())
         .expect("Error!");
-        contents.push_str(&file_content)
-
+        contents.push_str(&file_content);
+        contents.push_str("\n\n");
     }
 
     let file = File::create("./README.md");
